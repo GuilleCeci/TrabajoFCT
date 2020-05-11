7248f391bb705bc921bb373b89f133333bdd1873
= <=>
2020-05-11 12:32:46 +0200

    primera subida

---
+++ app/src/main/java/com/example/trabajofct/Activities/SecondActivity.java
@@ -0,0 +1,160 @@
+package com.example.trabajofct.Activities;
+
+
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import androidx.appcompat.widget.Toolbar;
+
+import com.example.trabajo1.R;
+import com.example.trabajofct.Modules.Usuarios;
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class SecondActivity extends AppCompatActivity {
+    private SharedPreferences preferences;
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private TextView NombreUsuario;
+    private DatabaseReference firebase;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_home);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        preferences = getSharedPreferences("Preferences", Context.MODE_PRIVATE);
+        drawerLayout = (DrawerLayout) findViewById(R.id.dlayout);
+        navigationView= (NavigationView) findViewById(R.id.navview);
+        NombreUsuario = (TextView) findViewById(R.id.nombreusuario);
+        firebase = FirebaseDatabase.getInstance().getReference();
+
+        firebase.child("Usuarios").addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                for(DataSnapshot objSnapshot : dataSnapshot.getChildren()) {
+                    Usuarios u = objSnapshot.getValue(Usuarios.class);
+                    if (u.getNombre()!=null || u.getEmail()!=null) {
+
+                        NombreUsuario.setText(u.getNombre()+ " " + u.getApellidos());
+
+                    }
+
+                }
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+        drawerLayout.addDrawerListener(new DrawerLayout.DrawerListener() {
+            @Override
+            public void onDrawerSlide(@NonNull View drawerView, float slideOffset) {
+
+            }
+
+            @Override
+            public void onDrawerOpened(@NonNull View drawerView) {
+
+            }
+
+            @Override
+            public void onDrawerClosed(@NonNull View drawerView) {
+
+            }
+
+            @Override
+            public void onDrawerStateChanged(int newState) {
+
+            }
+        });
+
+    }
+
+    public void setSupportActionBar(Toolbar toolbar) {
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+            case R.id.menu_logout:
+                logOut();
+                return true;
+            case R.id.menu_forget_logout:
+                removeSharedPreferences();
+                logOut();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+
+    private void logOut() {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
+
+    private void removeSharedPreferences() {
+        preferences.edit().clear().apply();
+    }
+}
